36518a139741941d058dd000616e634c411fd562
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-09 21:34:16 +0900

    20220209

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
 import express from "express";
 
+ const PORT = 4000;
  const app = express();
+
+ app.get("/", (req, res) => {
+ return res.send("I still love you");
+}
+ )
+ app.get("/login", (req,res) => {
+     return res.send("Login");
+ })
+
+
+const handleListening = () => console.log("Server");
+
+ app.listen(PORT, handleListening);
