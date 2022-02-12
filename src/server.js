f64f110e7e3817264d15d635fdb874c03d0c60bb
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-12 14:42:26 +0900

    20220212

--- src/server.js
+++ src/server.js
@@ -1,29 +1,36 @@
 import express from "express";
 import morgan from "morgan";
-import logger from "morgan";
 
- const PORT = 4000;
- const app = express();
-
- 
-
+const PORT = 4000;
+const app = express();
 const logger = morgan("dev")
+app.use(logger);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Videos");
+
+videoRouter.get("/watch", handleWatchVideo);
 
 
-
-const home = (req, res) => {
-    return res.send("hello");
-}
-
-const login = (req, res) => {
-    return res.send("login");
-}
- app.use(logger);
- app.get("/", home);
- app.get("/login", login);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 
 
 const handleListening = () => console.log("Server");
 
- app.listen(PORT, handleListening);
+app.listen(PORT, handleListening);
