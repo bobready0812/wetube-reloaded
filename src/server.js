0927d8fbab59384ad9b9c11e5f8dc73e721f4099
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-12 14:53:54 +0900

    20220212

--- src/server.js
+++ src/server.js
@@ -1,28 +1,29 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./router.js/globalRouter";
+import userRouter from "./router.js/userRouter";
+import videoRouter from "./router.js/videoRouter";
 
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev")
 app.use(logger);
 
-const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("home");
 
-globalRouter.get("/", handleHome);
 
-const userRouter = express.Router();
 
-const handleEditUser = (req, res) => res.send("Edit User");
 
-userRouter.get("/edit", handleEditUser);
 
-const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Videos");
 
-videoRouter.get("/watch", handleWatchVideo);
+
+
+
+
+
+
+
 
 
 app.use("/", globalRouter);
