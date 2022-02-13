f31c29bf8bff3c4f4eace247785ce5e07e0b5319
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-13 20:12:02 +0900

    20220213

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,4 +2,6 @@ export const join = (req, res) => res.send("join");
 
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send ("delete user");
-
+export const login = (req,res) => res.send("Login");
+export const see = (req, res) => res.send("Log out");
+export const logout = (req, res) => res.send("See User");
--- src/router.js/globalRouter.js
+++ src/router.js/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { trending } from "../controllers/userControllers";
-import { join } from "../controllers/videoController";
+import { trending , search} from "../controllers/userControllers";
+import { join, login } from "../controllers/videoController";
 
 
 const globalRouter = express.Router();
@@ -9,6 +9,8 @@ const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 
 export default globalRouter;
