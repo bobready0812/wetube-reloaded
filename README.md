bc56abd2251415ae2d7bdf76edaf9610acc33e12
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-14 20:15:10 +0900

    20220214

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 / -> Home
 /join -> Join
+/login -> Login
 /search ->Search
 
 
 
-/users/edit -> Edit Profile
-/users/delete -> Delete Profile
+/users/:id -> See user
+/users/logout -> Log out
+/users/edit -> Edit My profile
+/users/delete -> Delete User
 
 
 
 
-/videos/watch -> Watch Video
-/videos/edit -> Edit Video
-/videos/delete -> Delete Video
+/videos/:id -> Watch Video
+/videos/:id/edit -> Edit Video 
+/videos/upload -> Upload video
+/videos/:id/delete -> Delete Video
 /videos/comments  -> Comment on a video
 /videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,31 +1,17 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./router.js/globalRouter";
-import userRouter from "./router.js/userRouter";
-import videoRouter from "./router.js/videoRouter";
+import globalRouter from "./router/globalRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
 
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev")
+
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
