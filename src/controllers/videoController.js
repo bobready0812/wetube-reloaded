243adbb1803a32511b88cacbb73048f60485400b
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-16 20:06:52 +0900

    20220216

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,33 @@
-const fakeUser = {
-   username: "Nicolas",
-   loggedIn: true,
+
+export const trending = (req, res) => {
+   const videos = [
+      { 
+         title:"First video",
+         rating:5,
+         comments:2,
+         createdAt:"2 minutes ago",
+         views:59,
+         id:1
+      },
+      { 
+         title:"Second video",
+         rating:5,
+         comments:2,
+         createdAt:"2 minutes ago",
+         views:59,
+         id:1
+      },
+      { 
+         title:"Third video",
+         rating:5,
+         comments:2,
+         createdAt:"2 minutes ago",
+         views:59,
+         id:1
+      },
+   ];
+   return res.render("home", {pageTitle: "Home", videos});
 }
-
-
-
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser}); 
 
 export const see = (req, res) => {
    res.render("watch");
