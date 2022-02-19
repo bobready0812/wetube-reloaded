ec427d13d6145438d1342f06b2adf555e54e61a3
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-19 21:31:29 +0900

    20220219

--- src/db.js
+++ src/db.js
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube"m);
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+    useNewUrlParser: true, 
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log("DB Error", error));
+
+const handleOpen = () => console.log("Connected to DB");
+db.once("open", handleOpen);
 
 
 
