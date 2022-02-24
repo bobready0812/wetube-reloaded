6af3d613e8f17a2a779e8b1f1bce6ca132b83af1
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-24 19:37:30 +0900

    20220224

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
 });
 
 const db = mongoose.connection;
-db.on("error", (error) => console.log("DB Error", error));
+db.on("error", (err) => console.log("DB Error", err));
 
 const handleOpen = () => console.log("Connected to DB");
 db.once("open", handleOpen);
