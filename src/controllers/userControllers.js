98363ceb0133e8da243472308a5964f118a870c2
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-03 18:32:49 +0900

    20220303

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,18 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
 
+
+export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
+export const postJoin = async(req,res) => {
+    const {name, username, email, password, location} = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+}
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send ("delete user");
 export const login = (req,res) => res.send("Login");
---
+++ src/router/rootRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { home, search } from "../controllers/videoController";
+import { login, getJoin,postJoin } from "../controllers/userControllers";
+
+const rootRouter = express.Router();
+
+
+
+rootRouter.get("/", home);
+rootRouter.route("/join").get(getJoin).post(postJoin)
+rootRouter.get("/login", login);
+rootRouter.get("/search", search)
+
+
+export default rootRouter;
